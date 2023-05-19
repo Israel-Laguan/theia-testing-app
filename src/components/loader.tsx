ff17c4113bdfd3afd92ffe6b623ddae9e9404f6d
Allislove <iam@andresromana.com>
2023-05-19 12:35:47 -0500

    added loader

---
+++ src/components/loader.tsx
@@ -0,0 +1,14 @@
+import { InfinitySpin } from  'react-loader-spinner'
+
+
+
+export const Loader = () => {
+    return (
+        <div className="loader">
+            <InfinitySpin 
+                width='200'
+                color="#4fa94d"
+            />
+        </div>
+    )
+}
